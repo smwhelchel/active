a3a4a3bd3f4e86621110aff73b37f7d69ad38acf
Shannon <smwhelchel@gmail.com>
2016-01-27 10:15:19 -0800

    getting values from form inputs, #1

--- public/js/form.js
+++ public/js/form.js
@@ -1,7 +1,32 @@
-var data = document.getElementById('search-button').addEventListener('click', function(e) {
+/*var data = document.getElementById('search-button').addEventListener('click', function(e) {
   e.preventDefault();
   var data = quantity;
   var xhr = new XMLHttpRequest();
   xhr.open('POST', 'http://localhost:1337/search', true);
   xhr.send(data);
-}, false);
+}, false);*/
+
+//var clickSubmit = document.
+
+var activityData;
+var cityData;
+var stateData;
+var submitForm = document.getElementById('search-button');
+submitForm.addEventListener('click', function(e) {
+  e.preventDefault();
+  activityData = document.getElementById('activity').value;
+  var newActivity = activityData.replace(/\s+/g, '%20');
+  console.log(newActivity);
+  cityData = document.getElementById('city').value;
+  var newCity = cityData.replace(/\s+/g, '%20');
+  console.log(newCity);
+  stateData = document.getElementById('state').value;
+  var newState = stateData.toUpperCase();
+  console.log(newState);
+})
+
+var populate = document.getElementById('search-results')
+var clickButton = document.getElementById('search-button')
+clickButton.addEventListener('click', function() { 
+populate.className = 'show';
+})
