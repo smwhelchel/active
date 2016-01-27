6d5b9596b2f4f280de4125314b5b52247a0f424c
Shannon <smwhelchel@gmail.com>
2016-01-26 16:21:02 -0800

    updating map

--- public/js/app.js
+++ public/js/app.js
@@ -8,8 +8,8 @@ xhr.onreadystatechange = function() {
 function initMap() {
   var mapDiv = document.getElementById('map');
   var map = new google.maps.Map(mapDiv, {
-    center: {lat:44.450, lng: -78.546},
-    zoom:5,
+    center: {lat:33.600023, lng: -117.671995},
+    zoom:8,
     scrollwheel: false
   });
 }
