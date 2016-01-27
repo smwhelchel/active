56cfc8822bc7bbf5ec4f26bdc3fd671406456726
Shannon <smwhelchel@gmail.com>
2016-01-26 16:13:13 -0800

    creating server and populating map

---
+++ post.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var textParser = bodyParser.text();
+var router = express.Router();
+
+var baseUrl = 'http://api.amp.active.com/v2/search?query=';
+
+var apiKey = '&api_key=wwzz7ez5yn8s3vqzyns3hsxv';
+
+router.post('/', bodyParser.json(), function (req, res) {
+  var data = req.body;
+    var newCity = data.city.replace(/\s+/g, '%20');
+    var newState = data.state.toUpperCase();
+
+  var queryURL = baseUrl + data.activity + '&near=' + newCity + ',' + newState + ',US' + apiKey;
+
+  request(queryURL, function (error, response, body) {
+     console.log(body);
+    if (error) {
+      return console.log('error');
+    }
+
+    if (response.statusCode !== 200) {
+      return console.log('status code wrong');
+    }
+
+    if (!error && response.statusCode == 200) {
+      console.log('server works');
+      var data = JSON.parse(body);
+      res.send(data);
+    } 
+  });
+});
+
+module.exports = router;
--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,16 @@
-var express = require('express');
-var request = require('request');
-var bodyParser = require('body-parser');
-var textParser = bodyParser.text();
-var router = express.Router();
+/*var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+  if (xhr.readyState==4) {
 
-var baseUrl = 'http://api.amp.active.com/v2/search?query=';
+  }
+}*/
 
-var apiKey = '&api_key=wwzz7ez5yn8s3vqzyns3hsxv';
-
-router.post('/', bodyParser.json(), function (req, res) {
-  var data = req.body;
-    var newCity = data.city.replace(/\s+/g, '%20');
-    var newState = data.state.toUpperCase();
-
-  var queryURL = baseUrl + data.activity + '&near=' + newCity + ',' + newState + ',US' + apiKey;
-
-  request(queryURL, function (error, response, body) {
-     console.log(body);
-    if (error) {
-      return console.log('error');
-    }
-
-    if (response.statusCode !== 200) {
-      return console.log('status code wrong');
-    }
-
-    if (!error && response.statusCode == 200) {
-      console.log('server works');
-      var data = JSON.parse(body);
-    } 
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    center: {lat:44.450, lng: -78.546},
+    zoom:5,
+    scrollwheel: false
   });
-});
-
-module.exports = router;
-
+}
 
