174505605b0b08a35c1f1dc4e9040b9f2f532051
Shannon <smwhelchel@gmail.com>
2016-02-01 14:30:59 -0800

    adding images to readme

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 A website for fitness enthusiasts who want to find fitness events and activities in their area.
 
 Delpoyed to heroku at https://active-project.herokuapp.com/
+<img src="public/images/screenshot1.png">
+<img src="public/images/screenshot2.png">
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var gulp    = require('gulp');
-var mocha   = require('gulp-mocha');
 var nodemon = require('gulp-nodemon');
 var concat  = require('gulp-concat');
-var print   = require('gulp-print');
 var uglify = require('gulp-uglify');
 
 var app = 'server.js';
